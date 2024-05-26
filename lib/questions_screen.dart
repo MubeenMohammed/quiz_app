8c29a42286db335c22c1b2d05b7d614e8d4fa36d
MubeenMohammed <mubeen.mohammed@mail.mcgill.ca>
2024-05-26 00:49:37 -0400

    adding the intial quiz files

---
+++ lib/questions_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/models/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
+  @override
+  State<QuestionsScreen> createState() {
+    return _QuestionsScreenState();
+  }
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  int questionNo = 0;
+
+  @override
+  Widget build(context) {
+    final currentQuestion = questions[questionNo];
+
+    void answerSelected() {
+      if (questionNo < questions.length - 1) {
+        setState(() {
+          questionNo++;
+        });
+      }
+    }
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                currentQuestion.text,
+                style: GoogleFonts.lato(
+                  color: const Color.fromARGB(255, 187, 215, 251),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ...currentQuestion.getShuffledAnswers().map((item) {
+                return Container(
+                  margin: const EdgeInsets.only(bottom: 5),
+                  child: AnswerButton(
+                    answerText: item,
+                    onTap: answerSelected,
+                  ),
+                );
+              }),
+            ]),
+      ),
+    );
+  }
+}
---
+++ lib/quiz.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  // Widget? activeScreen;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   activeScreen = StartScreen(switchScreen);
+  // }
+
+  // void switchScreen() {
+  //   setState(() {
+  //     activeScreen = const QuestionsScreen();
+  //   });
+  // }
+
+  // another way of diplaying the conditional screen
+  var activeScreen = 'start-screen';
+  final List<String> selectedAnswers = [];
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          // child: activeScreen == 'start-screen'
+          //     ? StartScreen(switchScreen)
+          //     : const QuestionsScreen(),
+
+          // Another way of conditional statements
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
